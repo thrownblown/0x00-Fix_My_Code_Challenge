449e1fcb233cdf33ac6fcde2da7422e465138c61
Alex Farioletti <gsf.nyc@gmail.com>
2019-03-03 22:40:51 -0800

    betty like docs

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""
+Area of the square
+"""
 
 
 class Square:
+    """ Area of the square """
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Area of the square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +20,16 @@ class Square:
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Area of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Area of the square """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """ Area of the square """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
